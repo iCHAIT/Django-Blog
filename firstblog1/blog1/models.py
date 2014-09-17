517c9363d1a8080694f76bfc5bb6ca865f8f4ab1
iCHAIT <cgupta319@gmail.com>
2014-09-17 22:40:38 +0530

    models.py edited

--- firstblog1/blog1/models.py
+++ firstblog1/blog1/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class posts(models.Model):
     author = models.CharField(max_length = 30)
-    subject = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 100)
     bodytext = models.TextField()
     timestamp = models.DateTimeField()
