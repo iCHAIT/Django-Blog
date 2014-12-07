182ffb6397c897989ca72d517b55aafb7b8d0caf
iCHAIT <cgupta319@gmail.com>
2014-12-08 00:15:24 +0530

    minor changes

--- firstblog1/blog1/admin.py
+++ firstblog1/blog1/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+
 #for importing tables
+
 from blog1.models import posts
+
 # Register your models here.
 
 admin.site.register(posts)
--- firstblog1/firstblog1/urls.py
+++ firstblog1/firstblog1/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 #uncomment the next 2 lines to enable the admin
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,7 +10,9 @@ urlpatterns = patterns('',
     url(r'^$', 'blog1.views.home', name='home'),
                        #url(r'^blog/', include('blog.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
     #uncomment the next line to enable the admin
+    
     url(r'^admin/', include(admin.site.urls)),
                        
 )
